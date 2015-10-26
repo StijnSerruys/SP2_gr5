c84d207c3caf0cdad73ddb137e7432a92e21fe51
mouaad.amsalkhir <mouaad.amsalkhir@1a3611a1-9990-174a-893d-a94ac8705189>
2015-10-26 20:56:17 +0000

    Login functionaliteiten

---
+++ src/logins/ClickedListener.java
@@ -0,0 +1,13 @@
+package logins;
+import javax.swing.*;
+
+
+public class ClickedListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		L
+		
+	}
+
+}
--- src/logins/LoginDAO.java
+++ src/logins/LoginDAO.java
@@ -12,7 +12,7 @@ public class LoginDAO {
     private static Statement command;
     private static  ResultSet data;
 
-    public static void getUserName(String username){
+    public static String getUserName(String username){
         try {
             connection = DriverManager.getConnection(connectionString, username, password);
             command = connection.createStatement();
@@ -28,15 +28,18 @@ public class LoginDAO {
                 if (data.first()) {
                     while (data.next()) {
                         System.out.println("name= " + data.getString(2) + "");
+                        String username1 = data.getString(1);
+                        
                     }
                 }
+                return username;
             }catch(SQLException e){
                     e.printStackTrace();
             }
         }
     }
     
-    public static void getWachtwoord(String username){
+    public static String getWachtwoord(String username){
         try {
             connection = DriverManager.getConnection(connectionString, username, password);
             command = connection.createStatement();
@@ -51,13 +54,41 @@ public class LoginDAO {
             try {
                 if (data.first()) {
                     while (data.next()) {
-                        System.out.println("name= " + data.getString(2) + "");
+                      return data.getString(2);
                     }
                 }
             }catch(SQLException e){
                     e.printStackTrace();
             }
         }
+    }
+    public static Login getLoginDetails(String username)
+    {
+    	try {
+            connection = DriverManager.getConnection(connectionString, username, password);
+            command = connection.createStatement();
+            command.execute("Select wachtwoord from users where username="+username);
+            //command.execute("DELETE FROM EMPLOYEE WHERE first_name = 's'");
+            System.out.println("Executed.");
+            connection.close();
+        }catch (SQLException e){
+            System.out.println(e);
+            e.printStackTrace();
+        } finally{
+            try {
+                if (data.first()) {
+                    while (data.next()) {
+                      Login logUserIn= new Login(data.getInt(1),
+                    		  data.getInt(2),data.getString(3),data.getString(4),
+                    		  data.getString(5),data.getString(6),data.getInt(7));
+                    }
+                }
+            }catch(SQLException e){
+                    e.printStackTrace();
+            }
+        }
+    	
+    	
     }
     
 
