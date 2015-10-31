dc2c95de53e8cdab1033e8d44b93f72cee01ac63
stijn.serruys <stijn.serruys@1a3611a1-9990-174a-893d-a94ac8705189>
2015-10-31 16:35:40 +0000

    DAOs aangepast zodat we nu 2 enkele functies gebruiken voor connectie  met de db.

--- src/gui/Loginscr.java
+++ src/gui/Loginscr.java
@@ -1,6 +1,5 @@
 package gui;
 
-import dao.LoginDAO;
 
 import java.awt.EventQueue;
 
@@ -12,36 +11,39 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JOptionPane;
 import java.awt.event.*;
+import javax.swing.JPasswordField;
 
 public class Loginscr implements ActionListener {
 
 	private JFrame frame;
 	private JTextField textField;
-	private JTextField textField_1;
 	private JButton btnNewButton_1;
 	private JButton btnNewButton;
 	private JOptionPane JOptionpane;
+	private JPasswordField textField_1;
 
 	/**
 	 * Launch the application.
+	 * @wbp.parser.entryPoint
 	 */
-	public void start() {
+	public Loginscr()
+	{
+		initialize();
+	}
+
+	public static void start() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					Loginscr window = new Loginscr();
-					window.frame.setVisible(true);//NullpoinerExcep ?
+					window.frame.setVisible(true);
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
-
-	/**
-	 * Create the application.
-	 */
-
 
 	/**
 	 * Initialize the contents of the frame.
@@ -55,67 +57,60 @@ public class Loginscr implements ActionListener {
 		btnNewButton = new JButton("Login");
 		btnNewButton.setBounds(208, 200, 97, 25);
 		frame.getContentPane().add(btnNewButton);
-		
-		
-		
-				
-		
+
 		JButton btnNewButton_1 = new JButton("Cancel");
 		btnNewButton_1.setBounds(323, 200, 97, 25);
 		frame.getContentPane().add(btnNewButton_1);
-		
+
 		JLabel lblPrReadyLogon = new JLabel("PR ready logon");
 		lblPrReadyLogon.setBounds(162, 13, 130, 16);
 		frame.getContentPane().add(lblPrReadyLogon);
-		
+
 		textField= new JTextField();
 		textField.setBounds(208, 71, 116, 22);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblUser = new JLabel("User");
 		lblUser.setBounds(140, 74, 56, 16);
 		frame.getContentPane().add(lblUser);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(208, 129, 116, 22);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
+
 		JLabel lblPassword = new JLabel("Password");
 		lblPassword.setBounds(140, 132, 56, 16);
 		frame.getContentPane().add(lblPassword);
-		
-		
+
+		textField_1 = new JPasswordField();
+		textField_1.setBounds(208, 129, 116, 22);
+		frame.getContentPane().add(textField_1);
+		btnNewButton.addActionListener(this);
+		btnNewButton_1.addActionListener(this);
+
 
 	}
-	
-
-String getFilledinInfo(JTextField j1)
-{
-	 return j1.getText();
-}
-
-@Override
-public void actionPerformed(ActionEvent e) {
-	if (e.getSource()==btnNewButton_1)
+	String getFilledinInfo(JTextField j1)
 	{
-		frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+		return j1.getText();
 	}
-	else if (e.getSource()==btnNewButton)
-	{
-		if(LoginDAO.getWachtwoord(textField.getText())!=null)
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource()==btnNewButton_1)
 		{
-		LoginDAO.getLoginDetails(textField.getText());
+			frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 		}
-		else 
+		else if (e.getSource()==btnNewButton)
 		{
-			JOptionpane.showMessageDialog(null,"Username or password wrong");
+			if(dao.LoginDAO.getWachtwoord(textField.getText())!=null)
+			{
+				String ww = dao.LoginDAO.getWachtwoord(textField.getText());
+				System.out.println(ww);
+			}
+			else
+			{
+				JOptionpane.showMessageDialog(null,"Username or password wrong");
+			}
 		}
+
 	}
-	
-}
-
-
 }
 
